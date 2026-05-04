907c573c1c4d7d077b5ce6d82cc14fd825244b5b
nouhaila-sketch <nouhailaelbathi2@gmail.com>
2026-05-04 23:00:45 +0100

    Reformat views.py for consistency

--- gestion/views.py
+++ gestion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Ligne, Horaire, Bus
 
-# الصفحة الرئيسية (اختياري)
+
 def home(request):
     Lignes =Ligne.objects.all()
     Horaires = Horaire.objects.all()
